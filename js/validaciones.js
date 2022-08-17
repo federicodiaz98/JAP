a71c9351779bb9e185eff6eff972bb527691926f
federico.diaz <106123957+federicodiaz98@users.noreply.github.com>
2022-08-17 12:50:36 -0300

    cas

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,18 +1,23 @@
   var email = document.getElementById("email");
   var password = document.getElementById("password");
   const boton = document.getElementById("boton");
-  boton.addEventListener("click", submitCheck);
+
+boton.addEventListener("click", submitCheck);
 
 function submitCheck() {
   if (password.value.length > 0 && email.value.length > 0) {
     console.log("exito");
-    redirigir();
+    redirigir()
   } else {
    console.log("fail");
   }
   
 }
 
-function redirigir() {
-  location.replace("https://www.google.com/search?q=pinwilly&oq=pinwilly&aqs=chrome..69i57.5512j0j4&sourceid=chrome&ie=UTF-8");
+// function redirigir() {
+//   location.replace("https://www.google.com/search?q=pinwilly&oq=pinwilly&aqs=chrome..69i57.5512j0j4&sourceid=chrome&ie=UTF-8");
+// }
+
+  function redirigir() {
+  location.href = "https://www.google.com/search?q=pinwilly&oq=pinwilly&aqs=chrome..69i57.5512j0j4&sourceid=chrome&ie=UTF-8"; 
 }
